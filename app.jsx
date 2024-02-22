891c516960a58031331c11bcc49de00118a40566
rajesh8939 <114079773+rajesh8939@users.noreply.github.com>
2024-02-23 04:13:25 +0530

    Create app.jsx

---
+++ app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Counter from './Counter'; // Assuming Counter component is in the same directory
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Counter />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
